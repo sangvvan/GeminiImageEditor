29f09b9867c2da4c2e70d68017a3ef499ef108e6
Sang Vo <sangvvan@gmail.com>
2025-06-19 21:09:45 +0700

    Initial Commit

---
+++ GeminiImageEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeminiImageEditor
+//
+//  Created by Sang Vo on 19/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeminiImageEditor/GeminiImageEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiImageEditorApp.swift
+//  GeminiImageEditor
+//
+//  Created by Sang Vo on 19/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GeminiImageEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
